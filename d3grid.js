a02af7917d4302cb9c9e5dc9d666a6ff332eac14
connor <connor@cs.brown.edu>
2013-11-18 14:15:14 -0500

    Change grid color to be a little lighter

--- d3grid.js
+++ d3grid.js
@@ -7,15 +7,23 @@ function makeGrid(container, data) {
       sqLen = data.squareLen,
       sqSpace = data.spacing;
 
-  var svg = container.append('svg')
-          .attr('height', numRows*(sqLen+sqSpace) - sqSpace)
-          .attr('id', 'grid')
-          .attr('width', numCols*(sqLen+sqSpace) - sqSpace)
-          .style('border', '1px solid #000')
-          .style('display', 'inline-block')
-          .style('padding', 10);
+  var gridFieldHeight = numRows*(sqLen+sqSpace) - sqSpace,
+      gridFieldWidth = numCols*(sqLen+sqSpace) - sqSpace,
+      margin = {'bottom': 5, 'left': 5, 'right': 5, 'top': 5},
+      fullWidth = gridFieldWidth + margin.left + margin.right,
+      fullHeight = gridFieldHeight + margin.top + margin.bottom;
 
-  svg.selectAll('rect')
+  var borderColor = '#888';
+
+  var svg = container.append('svg')
+          .attr('height', fullHeight)
+          .attr('id', 'grid')
+          .attr('width', fullWidth)
+          .style('border', '1px solid '+borderColor)
+          .style('display', 'inline-block'),
+      rectG = svg.append('g').attr('transform', 'translate('+margin.left+','+margin.top+')');
+
+  rectG.selectAll('rect')
       .data(grid)
       .enter()
       .append('rect')
@@ -32,4 +40,22 @@ function makeGrid(container, data) {
         .style('fill', function(d) {
           return colorSet[d.color];
         });
+
+  var horizLineData = [{'x': 0, 'y': fullHeight/2}, {'x': fullWidth, 'y': fullHeight/2}],
+      vertLineData = [{'x': fullWidth/2, 'y': 0}, {'x': fullWidth/2, 'y': fullHeight}];
+
+  var lineFn = d3.svg.line()
+          .x(function(d) { return d.x; })
+          .y(function(d) { return d.y; })
+          .interpolate('linear'),
+      horizLine = svg.append('path')
+          .attr('d', lineFn(horizLineData))
+          .attr('stroke', borderColor)
+          .attr('stroke-width', 1)
+          .attr('fill', 'none'),
+      vertLine = svg.append('path')
+          .attr('d', lineFn(vertLineData))
+          .attr('stroke', borderColor)
+          .attr('stroke-width', 1)
+          .attr('fill', 'none');
 }
