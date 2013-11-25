9bc17a3f6ea188dca8c122d7ee52f2f74a647184
connor <connor@cs.brown.edu>
2013-11-25 14:01:20 -0500

    Refactor makeAllGrids

--- makeAllGrids.py
+++ makeAllGrids.py
@@ -1,30 +1,60 @@
 import json
 import gridData
 
-# last color is target
+def makeGrids(colors, grouped, numVariants):
+  # last color is target
+  dimensions = [6, 8, 10, 12, 14]
+  lengths = [10, 20, 30, 40, 50]
+  quadrants = ['tl', 'tr', 'br', 'bl']
+  spacing = 5
+
+  # makeGrid(grouped, numColors, numCols, numRows):
+  grid = gridData.makeGrid(False, 4, 6, 6)
+  grid = gridData.assignTgt(grid, 6, 6, 'tl', 4)
+
+  grids = []
+  for n in range(numVariants):
+    for d in dimensions:
+      for l in lengths:
+        for q in quadrants:
+          # makeGrid(grouped, numBaseColors, numCols, numRows)
+          grid = gridData.makeGrid(grouped, len(colors)-1, d, d)
+          # assignTgt(grid, numCols, numRows, quadrant, tgtColor)
+          grid = gridData.assignTgt(grid, d, d, q, len(colors)-1)
+          gridObj = {'grid': grid, 'length': l, 'setSize': d, 'tgtQuadrant': q, 'variant': n}
+          grids.append(gridObj)
+
+  output = {'colors': colors, 'grids': grids, 'lengths': lengths, 'setSizes':dimensions, 'spacing': spacing, 'numVariants': numVariants}
+  return output
+
 colors = ['rgb(120,106,24)','rgb(131,71,80)','rgb(23,73,95)','rgb(0,68,43)','rgb(98,63,117)']
-dimensions = [6, 8, 10, 12, 14]
-lengths = [10, 20, 30, 40, 50]
-numVariants = 4
-quadrants = ['tl', 'tr', 'br', 'bl']
-spacing = 5
+outputRand = makeGrids(colors, False, 4)
 
-# makeGrid(grouped, numColors, numCols, numRows):
-grid = gridData.makeGrid(False, 4, 6, 6)
-grid = gridData.assignTgt(grid, 6, 6, 'tl', 4)
+outputColor1 = ['rgb(120,106,24)','rgb(120,106,24)','rgb(120,106,24)','rgb(120,106,24)','rgb(98,63,117)']
+outputYellow = makeGrids(outputColor1, False, 1)
 
-grids = []
-for n in range(numVariants):
-  for d in dimensions:
-    for l in lengths:
-      for q in quadrants:
-        # makeGrid(grouped, numBaseColors, numCols, numRows)
-        grid = gridData.makeGrid(False, len(colors)-1, d, d)
-        # assignTgt(grid, numCols, numRows, quadrant, tgtColor)
-        grid = gridData.assignTgt(grid, d, d, q, len(colors)-1)
-        gridObj = {'grid': grid, 'length': l, 'setSize': d, 'tgtQuadrant': q, 'variant': n}
-        grids.append(gridObj)
+outputColor2 = ['rgb(131,71,80)','rgb(131,71,80)','rgb(131,71,80)','rgb(131,71,80)','rgb(98,63,117)']
+outputRed = makeGrids(outputColor2, False, 1)
 
-output = {'colors': colors, 'grids': grids, 'lengths': lengths, 'setSizes':dimensions, 'spacing': spacing, 'numVariants': numVariants}
+outputColor3 = ['rgb(23,73,95)','rgb(23,73,95)','rgb(23,73,95)','rgb(23,73,95)','rgb(98,63,117)']
+outputBlue = makeGrids(outputColor3, False, 1)
+
+outputColor4 = ['rgb(0,68,43)','rgb(0,68,43)','rgb(0,68,43)','rgb(0,68,43)','rgb(98,63,117)']
+outputGreen = makeGrids(outputColor4, False, 1)
+
+outputGrouped = makeGrids(colors, True, 4)
+
 with open('data/data.json', 'w') as outfile:
-  json.dump(output, outfile)
+  json.dump(outputRand, outfile)
+
+with open('data/dataYellow.json', 'w') as outfile:
+  json.dump(outputYellow, outfile)
+with open('data/dataRed.json', 'w') as outfile:
+  json.dump(outputRed, outfile)
+with open('data/dataBlue.json', 'w') as outfile:
+  json.dump(outputBlue, outfile)
+with open('data/dataGreen.json', 'w') as outfile:
+  json.dump(outputGreen, outfile)
+
+with open('data/dataGrouped.json', 'w') as outfile:
+  json.dump(outputGrouped, outfile)
