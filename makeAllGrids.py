b4d2093c35cb18cfa95ff81550aa688c6169da99
connor <connor@cs.brown.edu>
2013-11-25 13:36:48 -0500

    Debug refactored code to restore purple target

--- makeAllGrids.py
+++ makeAllGrids.py
@@ -21,42 +21,10 @@ for n in range(numVariants):
         # makeGrid(grouped, numBaseColors, numCols, numRows)
         grid = gridData.makeGrid(False, len(colors)-1, d, d)
         # assignTgt(grid, numCols, numRows, quadrant, tgtColor)
-        grid = gridData.assignTgt(grid, d, d, q, len(colors))
+        grid = gridData.assignTgt(grid, d, d, q, len(colors)-1)
         gridObj = {'grid': grid, 'length': l, 'setSize': d, 'tgtQuadrant': q, 'variant': n}
         grids.append(gridObj)
 
 output = {'colors': colors, 'grids': grids, 'lengths': lengths, 'setSizes':dimensions, 'spacing': spacing, 'numVariants': numVariants}
 with open('data/data.json', 'w') as outfile:
   json.dump(output, outfile)
-
-
-# start(grouped, numColors, numCols, numGrids, numRows)
-# Returns: {'grids': grids, 'numColors': numColors, 'numColumns': numCols, 'numRows': numRows }
-
-# six = [gridData.start(False, 4, 6, 1, 6) for i in range(len(lengths))]
-# eight = [gridData.start(False, 4, 8, 0, 8) for i in range(len(lengths))]
-# ten = [gridData.start(False, 4, 10, 0, 10) for i in range(len(lengths))]
-# twelve = [gridData.start(False, 4, 12, 0, 12) for i in range(len(lengths))]
-# fourteen = [gridData.start(False, 4, 14, 0, 14) for i in range(len(lengths))]
-
-# jsonSix = json.dumps(six)
-# jsonEight = json.dumps(eight)
-# jsonTen = json.dumps(ten)
-# jsonTwelve = json.dumps(twelve)
-# jsonFourteen = json.dumps(fourteen)
-
-
-# data = {'gridGroupSetSize': [jsonSix, jsonEight, jsonTen, jsonTwelve, jsonFourteen], 'colors': colors, 'lengths': lengths, 'spacing': spacing}
-
-# with open('data/data_6x6.json', 'w') as outfile:
-#   json.dump(six, outfile)
-# with open('data/data_8x8.json', 'w') as outfile:
-#   json.dump(eight, outfile)
-# with open('data/data_10x10.json', 'w') as outfile:
-#   json.dump(ten, outfile)
-# with open('data/data_12x12.json', 'w') as outfile:
-#   json.dump(twelve, outfile)
-# with open('data/data_14x14.json', 'w') as outfile:
-#   json.dump(fourteen, outfile)
-# with open('data/data.json', 'w') as outfile:
-#   json.dump(data, outfile)
