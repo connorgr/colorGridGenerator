f859c46be7e6a280fd8b34a6c2af381326e4ed1b
connor <connor@cs.brown.edu>
2013-12-02 12:27:55 -0500

    Changes

---
+++ mergeGridSources.py
@@ -0,0 +1,31 @@
+import os
+################################################################################
+# DIRECTORY NAMES
+
+# Random grids
+gridsRand = 'gridsRand'
+
+# Grouped grids
+gridsG1 = 'gridsG1'
+gridsG2 = 'gridsG2'
+gridsG3 = 'gridsG3'
+gridsG4 = 'gridsG4'
+
+# Single color grids
+gridsBlue = 'gridsBlue'
+gridsGreen = 'gridsGreen'
+gridsRed = 'gridsRed'
+gridsYellow = 'gridsYellow'
+
+dirs = [gridsRand, gridsG1, gridsG2, gridsG3, gridsG4, gridsBlue, gridsGreen,\
+    gridsRed, gridsYellow]
+
+################################################################################
+# SCRIPT
+
+for d in dirs:
+  files = os.listdir(d + '/')
+  for f in files:
+    if '.svg' not in f: continue
+    newName = f.replace('grid_', '')
+    os.rename(d + '/' + f, 'grids/' + d + '_' + newName)
