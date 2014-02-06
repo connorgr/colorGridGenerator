a75e05244a203b9c9ca10c66dd97bbac6889a749
connor <connor@cs.brown.edu>
2014-02-06 10:48:19 -0500

    Make changes to generation for double

---
+++ createDouble.sh
@@ -0,0 +1,6 @@
+./create.sh
+mv grids/* grids0
+
+./create.sh
+mv grids/* grids1
+python doubleGrids.py grids0/ grids1/
--- mergeGridSources.py
+++ mergeGridSources.py
@@ -23,6 +23,9 @@ dirs = [gridsRand, gridsG1, gridsG2, gridsG3, gridsG4, gridsBlue, gridsGreen,\
 ################################################################################
 # SCRIPT
 
+if not os.path.exists('grids'):
+  os.makedirs('grids')
+
 for d in dirs:
   files = os.listdir(d + '/')
   for f in files:
